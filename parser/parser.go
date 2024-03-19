9a8e3690fec828a986a775ce6ec422b11dede81b
unintendedfraud <briceunintendedfraud@fastmail.com>
2024-03-19 13:37:31 +0100

    parser

---
+++ parser/parser.go
@@ -0,0 +1,32 @@
+package parser
+
+import (
+	"gointerpreter/ast"
+	"gointerpreter/lexer"
+	"gointerpreter/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	currToken token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.currToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
